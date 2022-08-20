9364584c826a0ef9fbb9dadafaf9bb32a6906064
tizenmile <tizen48@gmail.com>
2022-08-20 13:23:30 +0300

    Update index.js

--- index.js
+++ index.js
@@ -17,6 +17,8 @@ let banks = [
   },
 ];
 
+let isEdit = false
+
 const newBank = document.querySelector("[data-creat-bank]");
 const modal = document.querySelector("[data-backdrop]");
 const btnCloseModal = document.querySelector("[ data-close-modal]");
@@ -55,13 +57,19 @@ const banksListUl = document.createElement("ul");
 banksListUl.classList.add("mortgage__banks-list");
 const divRootChildrenRef = document.querySelector(".mortgage__list");
 
+const createAddBank = document.createElement("button")
+createAddBank.setAttribute("type", "button")
+createAddBank.textContent = "Create new bank"
+createAddBank.classList.add("mortgage__btn-new-bank")
+divRootChildrenRef.append(banksListUl, createAddBank);
+createAddBank.addEventListener("click", openModal)
+
+
 const renderList = (banks) => {
   banksListUl.innerHTML = '';
   const banksMarkup = banks.map(renderItem).join("");
   banksListUl.insertAdjacentHTML("beforeend", banksMarkup);
-  divRootChildrenRef.append(banksListUl);
 };
-
 const renderItem = ({ name, id }, index) =>
   `<li class="mortgage__banks-item" data-id='${id}'>
                 <p class="mortgage__banks-name">${index + 1}. ${name}</p>
@@ -73,61 +81,73 @@ const renderItem = ({ name, id }, index) =>
 
 renderList(banks);
 
-const btnEdit = document.querySelectorAll(".btn-edit");
-btnEdit.forEach((button) => button.addEventListener("click", editBtn));
 
-
-
-function editBtn(evt) {
+function openModalBank(bank) {
   modal.classList.toggle("is-hidden");
-  const bankId = evt.target.closest(".mortgage__banks-item").dataset.id;
-  currentBank = banks.find((bank) => bank.id === bankId);
-  editBank(currentBank);
+  if (isEdit) {
+    editBank(bank);
+    return
+  }
 }
+
+editForm.addEventListener('submit', getChangedFormValue);
+
 
 function editBank(bank) {
   console.log(bank);
   const { elements } = editForm;
   console.log(elements);
   for (const key in bank) {
-  editForm.elements[key] ? (editForm.elements[key].value = bank[key]) : false;
+    editForm.elements[key] ? (editForm.elements[key].value = bank[key]) : false;
   }
-  
+
 }
 
-editForm.addEventListener('submit', getChangedFormValue);
+
 
 function getChangedFormValue(event) {
   event.preventDefault();
   modal.classList.toggle("is-hidden");
-  for (const key in currentBank) {
-    editForm.elements[key] ? ( currentBank[key] = editForm.elements[key].value ) : false;
-  }
-  console.log(currentBank);
 
-  banks.forEach(bank => {
-    if (bank.id === currentBank.id) {
-      for (const key in currentBank) {
-        bank[key] = currentBank[key];
+  if (isEdit) {
+    for (const key in currentBank) {
+      editForm.elements[key] ? (currentBank[key] = editForm.elements[key].value) : false;
+    }
+
+    banks.forEach(bank => {
+      if (bank.id === currentBank.id) {
+        for (const key in currentBank) {
+          bank[key] = currentBank[key];
         }
       }
-  })
-  console.log(banks);
+    })
+  } else {
+    const formData = new FormData(editForm)
+    const newBank = {}
+    formData.forEach((value, key) => {
+      newBank[key] = value
+    })
+    newBank.id = Date.now().toString()
+    banks.push(newBank)
+  }
+
   clearBanks();
   renderList(banks);
-
+  console.log(banks);
 }
 
 function clearBanks() {
-  
+
 }
 
 const mortgageItemEl = document.querySelectorAll(".mortgage__banks-item");
 
 banksListUl.addEventListener('click', (e) => {
-  const id = e.target.closest('.mortgage__banks-item').dataset.id
-  let currentBank = banks.find((bank) => bank.id === id);
   if (e.target.nodeName === 'UL') return
+
+  const id = e.target.closest('.mortgage__banks-item').dataset.id
+  currentBank = banks.find((bank) => bank.id === id);
+
   if (e.target.closest('.btn-close')) {
     banks = banks.filter(bank => bank.id !== id);
     renderList(banks);
@@ -135,10 +155,14 @@ banksListUl.addEventListener('click', (e) => {
     mortgageInfoBox.innerHTML = '';
     return
   }
-  if(e.target.closest('.mortgage__banks-item')) {
-     takeCurrentBank(currentBank)
+  if (e.target.closest(".btn-edit")) {
+    isEdit = true
+    openModalBank(currentBank)
   }
-  
+
+  if (e.target.closest('.mortgage__banks-item')) {
+    takeCurrentBank(currentBank)
+  }
 })
 
 const mortgageInfo = document.querySelector(".mortgage__info-box");
@@ -171,6 +195,6 @@ function takeCurrentBank(bank) {
       </li>
     </ul>
     `;
- mortgageInfo.innerHTML = template;
+  mortgageInfo.innerHTML = template;
 }
 
